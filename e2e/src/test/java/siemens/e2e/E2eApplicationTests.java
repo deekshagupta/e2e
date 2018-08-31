c20971cd2c1b752384674815909f1eca1fc10c74
Deeksha <er.deekshagupta07@gmail.com>
2018-08-31 17:32:53 +0530

    first commit

---
+++ e2e/src/test/java/siemens/e2e/E2eApplicationTests.java
@@ -0,0 +1,27 @@
+package siemens.e2e;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class E2eApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+	@Test
+	public void testVersion() {
+		assertEquals("", "");
+	}
+
+	@Test
+	public void testGetUserInfo() {
+		assertEquals("", "");
+	}
+}
